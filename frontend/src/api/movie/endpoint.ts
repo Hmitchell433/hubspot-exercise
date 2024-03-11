7fc322d0e53dc92c1040b26c7a69656345547420
Hannah Mitchell <hannah.mitchell433@gmail.com>
2024-03-11 12:38:40 -0700

    Updated variable names

--- frontend/src/api/movie/endpoint.ts
+++ frontend/src/api/movie/endpoint.ts
@@ -1,10 +1,10 @@
-import http from "api/axios";
+import axios from "api/axios";
 import { MOVIES_URL } from "constant";
 import { TokenType } from "types";
 
 export const getMovies = (token: TokenType, queryParams: string) =>
-  http.get(`${MOVIES_URL}?${queryParams}`, {
+  axios.get(`${MOVIES_URL}?${queryParams}`, {
     cancelToken: token?.token,
   });
 
-export const getFilterValues = () => http.get(`${MOVIES_URL}/filter-values`);
+export const getFilterValues = () => axios.get(`${MOVIES_URL}/filter-values`);
